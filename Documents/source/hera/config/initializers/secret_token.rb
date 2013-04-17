b0c161ab8446c3cc93f24834e7fc494605fffe0e
Keshu Jiang <jiangks@debian.(none)>
2013-04-18 00:08:50 +0800

    init code

---
+++ Documents/source/hera/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hera::Application.config.secret_key_base = '4008b161cd04996d0ff70e8ed7549a7f9a0c1fd3d56c8559cecd5c65f3f4263a70a9f49426dec59962c4be425a1ffc4da0d805c4c7cb4e9357d14c8c0365c692'
